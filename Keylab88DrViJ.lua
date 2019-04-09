dc6dbe62efee3e105c3de91808c16dfd5da5808a
dr-vij <propellerheadvij@gmail.com>
2019-04-09 21:28:45 +0300

    More midi answers are added to comment

--- Keylab88DrViJ.lua
+++ Keylab88DrViJ.lua
@@ -34,3 +34,34 @@ function remote_probe()
 		response=controlResponse
 	}
 end
+
+
+-- Knobs answers
+-- 3F - down
+-- 41 - up
+--In:  B0  4A  3F  |  Ch 1 CC 74 - Brightness P1
+-- In:  B0  47  3F  |  Ch 1 CC 71 - Timbre/Harmonic Intens. P2
+--In:  B0  4C  3F  |  Ch 1 CC 76 - Vibrato Rate P3
+--In:  B0  4C  41  |  Ch 1 CC 76 - Vibrato Rate
+-- In:  B0  4D  00  |  Ch 1 CC 77 - Vibrato Depth P4
+--  In:  B0  5D  05  |  Ch 1 CC 93 - Chorus Send Level P5
+
+--In:  B0  12  00  |  Ch 1 CC 18 - General Purpose Controller 3   P6
+--In:  B0  13  00  |  Ch 1 CC 19 - General Purpose Controller 4		P7
+--In:  B0  10  05  |  Ch 1 CC 16 - General Purpose Controller 1		P8	
+--In:  B0  11  06  |  Ch 1 CC 17 - General Purpose Controller 2		P9
+--In:  B0  5B  03  |  Ch 1 CC 91 - Reverb Send Level				P10
+
+-- In:  B0  23  00  |  Ch 1 CC 35 - Undefined				P1-2
+--In:  B0  24  03  |  Ch 1 CC 36 - Foot Controller LSB		p2-2
+--In:  B0  25  00  |  Ch 1 CC 37 - Portamento Time LSB		p3-2
+-- In:  B0  26  00  |  Ch 1 CC 38 - Data Entry LSB			p4-2
+--In:  B0  27  00  |  Ch 1 CC 39 - Channel Volume LSB		p5-2
+
+-- In:  B0  28  06  |  Ch 1 CC 40 - Balance LSB					p6-2
+-- In:  B0  29  00  |  Ch 1 CC 41 - Undefined					p7-2
+--  In:  B0  2A  00  |  Ch 1 CC 42 - Pan LSB					p8-2
+--In:  B0  2B  00  |  Ch 1 CC 43 - Expression Controller LSB	p9-2
+--In:  B0  2C  00  |  Ch 1 CC 44 - Effect Control 1 LSB 		p10-2
+
+
