5ce4bdddcbecfefe318528592e7419303fcb8664
dr-vij <propellerheadvij@gmail.com>
2019-04-10 00:38:46 +0300

    Faders and knobs signal parse

--- Keylab88DrViJ.lua
+++ Keylab88DrViJ.lua
@@ -2,30 +2,135 @@ function remote_init()
 	local items=
 	{
 		--Transport buttons
-		{name="Rewind", input="button"},
-		{name="FastForward", input="button"},
-		{name="Stop", input="button"},
-		{name="Play", input="button"},
-		{name="Record", input="button"},
-		{name="Loop", input="button"},
+		{name="Rewind", 		input="button"},
+		{name="FastForward", 	input="button"},
+		{name="Stop", 			input="button"},
+		{name="Play", 			input="button"},
+		{name="Record", 		input="button"},
+		{name="Loop", 			input="button"},
+		
+		--Knobs:
+		--Bank_1
+		{name="Cutoff_1", 		input="delta"},
+		{name="Resonance_1", 	input="delta"},
+		{name="LFORate_1", 		input="delta"},
+		{name="LFOAmount_1", 	input="delta"},
+		{name="Chorus_1", 		input="delta"},
+		{name="Param1_1", 		input="delta"},
+		{name="Param2_1", 		input="delta"},
+		{name="Param3_1", 		input="delta"},
+		{name="Param4_1", 		input="delta"},
+		{name="Delay_1", 		input="delta"},
+		
+		--Knobs:
+		--Bank_2
+		{name="Cutoff_2", 		input="delta"},
+		{name="Resonance_2", 	input="delta"},
+		{name="LFORate_2", 		input="delta"},
+		{name="LFOAmount_2", 	input="delta"},
+		{name="Chorus_2", 		input="delta"},
+		{name="Param1_2", 		input="delta"},
+		{name="Param2_2", 		input="delta"},
+		{name="Param3_2", 		input="delta"},
+		{name="Param4_2", 		input="delta"},
+		{name="Delay_2", 		input="delta"},
+		
+		--Faders:
+		--Bank_1
+		{name="Atack1_1", 		input="value", min=0, max=127},
+		{name="Decay1_1", 		input="value", min=0, max=127},
+		{name="Sustain1_1", 	input="value", min=0, max=127},
+		{name="Release1_1", 	input="value", min=0, max=127},
+
+		{name="Atack2_1", 		input="value", min=0, max=127},
+		{name="Decay2_1", 		input="value", min=0, max=127},
+		{name="Sustain2_1", 	input="value", min=0, max=127},
+		{name="Release2_1", 	input="value", min=0, max=127},
+		{name="Fader9_1", 		input="value", min=0, max=127},
+		
+		--Faders:
+		--Bank_2
+		{name="Atack1_2", 		input="value", min=0, max=127},
+		{name="Decay1_2", 		input="value", min=0, max=127},
+		{name="Sustain1_2", 	input="value", min=0, max=127},
+		{name="Release1_2", 	input="value", min=0, max=127},
+
+		{name="Atack2_2", 		input="value", min=0, max=127},
+		{name="Decay2_2", 		input="value", min=0, max=127},
+		{name="Sustain2_2", 	input="value", min=0, max=127},
+		{name="Release2_2", 	input="value", min=0, max=127},
+		{name="Fader9_2", 		input="value", min=0, max=127},
 	}
 	remote.define_items(items)
 	
 	local inputs=
 	{
 		--Transport buttons
-		{pattern="F0 7F 7F 06 05 F7", name="Rewind", value="1"},
-		{pattern="F0 7F 7F 06 04 F7", name="FastForward", value="1"},
-		{pattern="F0 7F 7F 06 01 F7", name="Stop", value="1"},
-		{pattern="F0 7F 7F 06 02 F7", name="Play", value="1"},
-		{pattern="F0 7F 7F 06 06 F7", name="Record", value="1"},
-		{pattern="B0 37 xx", name="Loop", value="1"},	--ToDo: Find a way to use switch as global value
+		{pattern="F0 7F 7F 06 05 F7", 	name="Rewind", 		value="1"},
+		{pattern="F0 7F 7F 06 04 F7", 	name="FastForward", value="1"},
+		{pattern="F0 7F 7F 06 01 F7", 	name="Stop", 		value="1"},
+		{pattern="F0 7F 7F 06 02 F7", 	name="Play", 		value="1"},
+		{pattern="F0 7F 7F 06 06 F7", 	name="Record", 		value="1"},
+		{pattern="B0 37 xx", 			name="Loop", 		value="1"},	--ToDo: Find a way to use switch as global value
+		
+		--Knobs:
+		--Bank_1
+		{pattern="B0 4A xx", name="Cutoff_1", 		value="(64 - x)* -1"},
+		{pattern="B0 47 xx", name="Resonance_1", 	value="(64 - x)* -1"},
+		{pattern="B0 4C xx", name="LFORate_1", 		value="(64 - x)* -1"},
+		{pattern="B0 4D xx", name="LFOAmount_1", 	value="(64 - x)* -1"},
+		{pattern="B0 5D xx", name="Chorus_1", 		value="(64 - x)* -1"},		
+		{pattern="B0 12 xx", name="Param1_1", 		value="(64 - x)* -1"},
+		{pattern="B0 13 xx", name="Param2_1", 		value="(64 - x)* -1"},
+		{pattern="B0 10 xx", name="Param3_1", 		value="(64 - x)* -1"},
+		{pattern="B0 11 xx", name="Param4_1", 		value="(64 - x)* -1"},
+		{pattern="B0 5B xx", name="Delay_1", 		value="(64 - x)* -1"},
+		
+		--Knobs:
+		--Bank_2
+		{pattern="B0 23 xx", name="Cutoff_2", 		value="(64 - x)* -1"},
+		{pattern="B0 24 xx", name="Resonance_2", 	value="(64 - x)* -1"},
+		{pattern="B0 25 xx", name="LFORate_2", 		value="(64 - x)* -1"},
+		{pattern="B0 26 xx", name="LFOAmount_2", 	value="(64 - x)* -1"},
+		{pattern="B0 27 xx", name="Chorus_2", 		value="(64 - x)* -1"},		
+		{pattern="B0 28 xx", name="Param1_2", 		value="(64 - x)* -1"},
+		{pattern="B0 29 xx", name="Param2_2", 		value="(64 - x)* -1"},
+		{pattern="B0 2A xx", name="Param3_2", 		value="(64 - x)* -1"},
+		{pattern="B0 2B xx", name="Param4_2", 		value="(64 - x)* -1"},
+		{pattern="B0 2C xx", name="Delay_2", 		value="(64 - x)* -1"},
+		
+		--Faders:
+		--Bank_1:
+		{pattern="b0 49 xx", name="Atack1_1"},
+		{pattern="b0 4B xx", name="Decay1_1"},
+		{pattern="b0 4F xx", name="Sustain1_1"},
+		{pattern="b0 48 xx", name="Release1_1"},
+
+		{pattern="b0 50 xx", name="Atack2_1"},
+		{pattern="b0 51 xx", name="Decay2_1"},
+		{pattern="b0 52 xx", name="Sustain2_1"},
+		{pattern="b0 53 xx", name="Release2_1"},
+		{pattern="b0 55 xx", name="Fader9_1"},
+		
+		--Faders:
+		--Bank_2
+		{pattern="b0 43 xx", name="Atack1_2"},
+		{pattern="b0 44 xx", name="Decay1_2"},
+		{pattern="b0 45 xx", name="Sustain1_2"},
+		{pattern="b0 46 xx", name="Release1_2"},
+
+		{pattern="b0 57 xx", name="Atack2_2"},
+		{pattern="b0 58 xx", name="Decay2_2"},
+		{pattern="b0 59 xx", name="Sustain2_2"},
+		{pattern="b0 5A xx", name="Release2_2"},
+		{pattern="b0 5C xx", name="Fader9_2"},
+		
 	}
 	remote.define_auto_inputs(inputs)
 end
 
 function remote_probe()
-	local controlRequest="F0 7E 7F 06 01 F7"
+	local controlRequest = "F0 7E 7F 06 01 F7"
 	-- for example: Keylab49: 	"F0 7E 00 06 02 00 20 6B 02 00 05 02 ?? ?? ?? ?? F7"
 	local controlResponse=		"F0 7E 00 06 02 00 20 6B 02 00 05 48 ?? ?? ?? ?? F7"
 	return 
@@ -34,34 +139,3 @@ function remote_probe()
 		response=controlResponse
 	}
 end
-
-
--- Knobs answers
--- 3F - down
--- 41 - up
---In:  B0  4A  3F  |  Ch 1 CC 74 - Brightness P1
--- In:  B0  47  3F  |  Ch 1 CC 71 - Timbre/Harmonic Intens. P2
---In:  B0  4C  3F  |  Ch 1 CC 76 - Vibrato Rate P3
---In:  B0  4C  41  |  Ch 1 CC 76 - Vibrato Rate
--- In:  B0  4D  00  |  Ch 1 CC 77 - Vibrato Depth P4
---  In:  B0  5D  05  |  Ch 1 CC 93 - Chorus Send Level P5
-
---In:  B0  12  00  |  Ch 1 CC 18 - General Purpose Controller 3   P6
---In:  B0  13  00  |  Ch 1 CC 19 - General Purpose Controller 4		P7
---In:  B0  10  05  |  Ch 1 CC 16 - General Purpose Controller 1		P8	
---In:  B0  11  06  |  Ch 1 CC 17 - General Purpose Controller 2		P9
---In:  B0  5B  03  |  Ch 1 CC 91 - Reverb Send Level				P10
-
--- In:  B0  23  00  |  Ch 1 CC 35 - Undefined				P1-2
---In:  B0  24  03  |  Ch 1 CC 36 - Foot Controller LSB		p2-2
---In:  B0  25  00  |  Ch 1 CC 37 - Portamento Time LSB		p3-2
--- In:  B0  26  00  |  Ch 1 CC 38 - Data Entry LSB			p4-2
---In:  B0  27  00  |  Ch 1 CC 39 - Channel Volume LSB		p5-2
-
--- In:  B0  28  06  |  Ch 1 CC 40 - Balance LSB					p6-2
--- In:  B0  29  00  |  Ch 1 CC 41 - Undefined					p7-2
---  In:  B0  2A  00  |  Ch 1 CC 42 - Pan LSB					p8-2
---In:  B0  2B  00  |  Ch 1 CC 43 - Expression Controller LSB	p9-2
---In:  B0  2C  00  |  Ch 1 CC 44 - Effect Control 1 LSB 		p10-2
-
-
